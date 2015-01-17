570ca98d752f42794539e434bc4bb545c55b2eb9
Johnie Hjelm <johnie.hjelm@symbio.com>
2015-01-17 14:28:23 +0100

    cli changes

--- cli.js
+++ cli.js
@@ -1,31 +1,31 @@
 #!/usr/bin/env node
 'use strict';
 
-var meow 		= require('meow');
-var pkg 		= require('./package.json');
-var IPinfo 	= require('./');
+var meow    = require('meow');
+var pkg     = require('./package.json');
+var IPinfo  = require('./');
 
 var cli = meow({
-	help: [
-		'',
-		'Usage',
-		'  $ ipinfo',
-		'',
-		'{',
-		'  "ip": "90.111.11.111"',
-		'  "hostname": "Hostname"',
-		'  "city": "Stockholm"',
-		'  "region": "Stockholms Lan"',
-		'  "country": "SE"',
-		'  "loc": "59.329323,18.068581"',
-		'  "org": "Adroiter AB"',
-		'  "postal": "1337"',
-		'}',
-		'',
-		'Options',
-		'  --ip    Specify an IP address',
-		'  --type  Type of information: ip|hostname|city|region|country|loc|org|postal'
-	].join('\n')
+  help: [
+    '',
+    'Usage',
+    '  $ ipinfo',
+    '',
+    '{',
+    '  "ip": "90.111.11.111"',
+    '  "hostname": "Hostname"',
+    '  "city": "Stockholm"',
+    '  "region": "Stockholms Lan"',
+    '  "country": "SE"',
+    '  "loc": "59.329323,18.068581"',
+    '  "org": "Adroiter AB"',
+    '  "postal": "1337"',
+    '}',
+    '',
+    'Options',
+    '  --ip    Specify an IP address',
+    '  --type  Type of information: ip|hostname|city|region|country|loc|org|postal'
+  ].join('\n')
 });
 
 /**
@@ -34,9 +34,9 @@ var cli = meow({
 var type = cli.flags.type;
 
 IPinfo(function (err, ip) {
-	if (err) {
-		throw err;
-	}
+  if (err) {
+    throw err;
+  }
 
-	console.log(type ? ip[type] : ip);
+  console.log(type ? ip[type] : ip);
 });
